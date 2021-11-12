1eadf2ef21da47829fd5680ba8885866822f6c36
Marián <Matiasus@users.noreply.github.com>
2021-11-12 07:47:38 +0100

    Update rcc.h
    Comments modification and remove HSI function

--- Library/rcc.h
+++ Library/rcc.h
@@ -1,26 +1,30 @@
 /** 
- * ---------------------------------------------------------------+ 
- * @desc        RCC
- * ---------------------------------------------------------------+ 
+ * --------------------------------------------------------------------------------------------+
+ * @desc        RCC Library for Blackpill (stm32f103c8t6)
+ * --------------------------------------------------------------------------------------------+
  *              Copyright (C) 2021 Marian Hrinko.
  *              Written by Marian Hrinko (mato.hrinko@gmail.com)
  *
  * @author      Marian Hrinko
  * @datum       21.04.2021
  * @file        rcc.h
- * @update      -
+ * @update      11.11.2021
  * @version     1.0
  * @tested      stm32f103c8t6
  *
- * @depend      
- * ---------------------------------------------------------------+
- * @interface   
- * @pins         
- *
+ * @depend      stm32f10x.h    
+ * --------------------------------------------------------------------------------------------+
+ * @descr       Select clock source - HSE (8MHz), PLL (72MHz)
+ * @note        
+ * --------------------------------------------------------------------------------------------+
+ * @inspir      Standard Peripheral Library 
  */
 
 #ifndef RCC_H
 #define RCC_H
+
+  // includes
+  #include <stm32f10x.h>
 
   /**
    * @desc    RCC deinit
@@ -48,15 +52,6 @@
    * @return  void
    */
   uint32_t SetSysClk_PLL_72MHz (void);
-
-  /**
-   * @desc    RCC init HSI
-   *
-   * @param   void
-   *
-   * @return  void
-   */
-  void RCC_Init_HSI (void);
 
   /**
    * @desc    RCC->CFGR Config
